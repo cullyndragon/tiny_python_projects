6b3b4feac2c67a8eebb0a4b9d84d4b01630814c7
cullyndragon <cullyn@outlook.com>
2023-01-13 10:46:54 -0500

    Complete Chapter 1

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,9 +1,31 @@
 #!/Users/cullyn/miniconda3/envs/tinypy/bin/python
-# Purpose: To say hello.
+"""
+Author: Scott Kliemann
+Purpose: To say hello.
+"""
+
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello!')
-parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+# =============================================
+def get_args():
+    """Get the command line arguements"""
+
+    parser = argparse.ArgumentParser(description="Say hello!")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
+    return parser.parse_args()
+
+
+# =============================================
+def main():
+    """Main function"""
+
+    args = get_args()
+    print("Hello, " + args.name + "!")
+
+
+# =============================================
+if __name__ == "__main__":
+    main()
